986de5696420688b2cc813ad22813039a87f09fe
Damian-Rios <damianjrios5@gmail.com>
2024-09-24 00:11:43 -0500

    Cleaned cvs data added functionality to create charts folder and added it to gitignore

--- main.py
+++ main.py
@@ -4,7 +4,16 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+from pathlib import Path
 
 movies = pd.read_csv("movie_info.csv")
 
-print(movies.head())
+# Convert data to floats
+movies['critic_score'] = movies['critic_score'].str.replace('%', '').astype(float)
+movies['audience_score'] = movies['audience_score'].str.replace('%', '').astype(float)
+
+# Code to create charts folder
+try:
+    Path("charts").mkdir()
+except FileExistsError:
+    pass
