a836bb4e5717da9c0c05795a8d88aaafcbf0e33b
Damian-Rios <damianjrios5@gmail.com>
2024-09-24 01:15:45 -0500

    created first chart/graph (bar chart) added comments

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from pathlib import Path
+import textwrap
 
 movies = pd.read_csv("movie_info.csv")
 
@@ -17,3 +18,29 @@ try:
     Path("charts").mkdir()
 except FileExistsError:
     pass
+
+# First chart (Bar Chart): Comparison of Critic and Audience Scores
+# number of random rows of data to choose
+sample_size = 15
+sample_movies = movies.sample(n=sample_size)
+
+# Getting data to plot
+sample_movies[['critic_score', 'audience_score']].plot(kind='bar', figsize=(12, 6))
+
+# Wrap movie titles using textwrap for better layout
+wrapped_titles = [textwrap.fill(title, width=15) for title in sample_movies['title']]
+
+# Setting up chart labels
+plt.title('Comparison of Critic and Audience Scores (Sampled)')
+plt.ylabel('Score (%)')
+plt.xlabel('Movies')
+plt.legend(title='Score Type')
+
+# Formating x-axis so it looks better
+plt.xticks(ticks=range(sample_size), labels=wrapped_titles, rotation=45, ha='center')
+
+plt.tight_layout()
+
+# Save plot as png
+plt.savefig('charts/critic_audience_comparison.png')
+plt.show()
